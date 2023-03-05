ced02b0fc5b7fc10f1a37543e29cff33944d6421
tusharravenclaw <tushar68053@gmail.com>
2023-03-05 11:58:28 +0530

    add password and e-mail validation

--- script.js
+++ script.js
@@ -1,43 +1,90 @@
-const fName = document.querySelector("#fname")
-const lName = document.querySelector("#lname")
-const phone = document.querySelector("#phone")
-const eMail = document.querySelector("#email")
-const fNameError = document.querySelector("#fname-error");
-const lNameError = document.querySelector("#lname-error")
-const phoneError = document.querySelector("#phone-error")
-const eMailError = document.querySelector("#email-error")
+const fName = document.querySelector("#fname");
+const lName = document.querySelector("#lname");
+const phone = document.querySelector("#phone");
+const eMail = document.querySelector("#email");
+const password = document.querySelector("#pwd");
+const passMatch = document.querySelector("#confirm-pwd");
+const button = document.querySelector("#button");
 
-fName.addEventListener("input", function(e){
-    if(!fName.value){
+const fNameError = document.querySelector("#fname-error");
+const lNameError = document.querySelector("#lname-error");
+const phoneError = document.querySelector("#phone-error");
+const eMailError = document.querySelector("#email-error");
+const passwordError = document.querySelector("#pwd-error");
+const passMatchError = document.querySelector("#confirm-pwd-error");
+
+fName.addEventListener("input", function (e) {
+    if (!fName.value) {
         fNameError.textContent = "Please write your full name";
-    } else if(containsNumbers(fName.value)){
+    } else if (containsNumbers(fName.value)) {
         fNameError.textContent = "Numbers are not allowed"
-    } else{
+    } else {
         fNameError.textContent = "";
     }
 });
 
-lName.addEventListener("input", function(e){
-    if(!lName.value){
+lName.addEventListener("input", function (e) {
+    if (!lName.value) {
         lNameError.textContent = "Please write your full name";
-    } else if(containsNumbers(lName.value)){
+    } else if (containsNumbers(lName.value)) {
         lNameError.textContent = "Numbers are not allowed"
-    } else{
+    } else {
         lNameError.textContent = "";
     }
 });
 
-phone.addEventListener("input", function(e){
-    if(isNaN(phone.value)) { 
+phone.addEventListener("input", function (e) {
+    if (isNaN(phone.value)) {
         phoneError.textContent = "Characters are not allowed"
-    } else if((phone.value).length != 10){
+    } else if ((phone.value).length != 10) {
         phoneError.textContent = "Please type a 10-digit phone number"
-    } else{
+    } else {
         phoneError.textContent = "";
     }
 });
 
+eMail.addEventListener("input", function (e) {
+    if (eMail.validity.patternMismatch) {
+        eMailError.textContent = "Enter a valid e-mail address"
+    } else {
+        eMailError.textContent = "";
+    }
+});
+
+password.addEventListener("input", function (e) {
+    passMatchCheck()
+
+    if (password.validity.patternMismatch) {
+        passwordError.textContent = `Must have at least 8 characters, 1 capital letter, and 1 number.`
+    } else {
+        passwordError.textContent = "";
+    }
+});
+
+passMatch.addEventListener("input", function (e) {
+    passMatchCheck()
+});
+
+function passMatchCheck() {
+    if (password.value != passMatch.value) {
+        passMatchError.textContent = "Password are not matching";
+        passMatch.classList.add("error-box");
+        button.disabled = true;
+    } else {
+        passMatchError.textContent = "";
+        button.disabled = false;
+        passMatch.classList.remove("error-box");
+    }
+}
+
+button.addEventListener("click", (e) => {
+    console.log("button")
+})
 
 function containsNumbers(str) {
     return /\d/.test(str);
+}
+
+function checkEmail(email) {
+    return email.match(/^\S+@\S+\.\S+$/);
 }
